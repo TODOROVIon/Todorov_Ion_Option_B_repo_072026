c5d641cfc5677a3ee71145cb0181d82482dc69f9
Todorov Ion <ion@atidgo.ai>
2026-08-07 15:57:00 +0200

    creating login controller+view

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -11,11 +11,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    #[Route('/inscription', name: 'app_inscription')]
+    public function inscription(Request $request, UserPasswordHasherInterface $userPasswordHasher,Security $security, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -28,15 +29,42 @@ class RegistrationController extends AbstractController
 
             $user->setApiAccess(false);
 
+            $security->login($user,'form_login');
+
             $entityManager->persist($user);
             $entityManager->flush();
 
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_home');
         }
 
-        return $this->render('registration/register.html.twig', [
+        return $this->render('registration/inscription.html.twig', [
             'registrationForm' => $form,
         ]);
     }
+
+     #[Route('/connexion', name: 'app_login')]
+      public function login(AuthenticationUtils $authenticationUtils): Response
+      {
+          if ($this->getUser()) {
+              return $this->redirectToRoute('app_home');
+          }
+
+          $error = $authenticationUtils->getLastAuthenticationError();
+
+          $lastUsername = $authenticationUtils->getLastUsername();
+
+          return $this->render('registration/connexion.html.twig', [
+              'last_username' => $lastUsername,
+              'error' => $error,
+          ]);
+      }
+
+      #[Route('/deconnexion', name: 'app_logout')]
+      public function logout(): void
+      {
+          // Cette méthode reste vide - Symfony gère la déconnexion automatiquement
+          throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+      }
+
 }
