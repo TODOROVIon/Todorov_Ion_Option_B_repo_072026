23e9211ed41739e7b4d0c7a8366147ad996baf5e
Todorov Ion <ion@atidgo.ai>
2026-09-11 15:45:40 +0200

    validate my wishlist

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -99,4 +99,26 @@ class PanierController extends AbstractController
         return $this->redirectToRoute('app_panier');
     }
 
+
+    #[Route('/panier/validation', name: 'app_panier_validation')]
+    public function validation(OrderRepository $orderRepo, EntityManagerInterface $em
+        ): Response
+    {
+        $user = $this->getUser();
+        $order = $orderRepo->findOneBy(['user'=>$user, 'status'=>'cart']);
+
+        if (!$order || $order->getOrderItems()->isEmpty()) {
+            $this->addFlash('error', 'Votre panier est vide.');
+            return $this->redirectToRoute('app_panier');
+        }
+
+        $order->setStatus('validated');
+        $order->setOrderDate(new \DateTimeImmutable());
+
+        $em->flush();
+
+        $this->addFlash('success','Voctre commande a été validée');
+
+        return $this->redirectToRoute('app_panier');
+    }
 }
